9d24b05f19dcea21afb5884fb03c54b738bb1ef8
benny.fenton <benny.fenton@googlemail.com>
2019-07-03 09:44:43 +0100

    Halfway done with adding client-side validation for log-in form, couldn't finish before being forced to commit and close the project

---
+++ SocialMediaProject/wwwroot/js/login.js
@@ -0,0 +1,11 @@
+﻿//JavaScript here
+$("#login-form").on("submit", function (e) {
+    if ($("#Email").val() === "") {
+        e.preventDefault();
+        $("#Email").addClass("is-invalid");
+    }
+    if ($("#Password").val() === "") { 
+        e.preventDefault();
+        $("#Password").addClass("is-invalid");
+    }
+});
